21d7f008770385d538e04956998a68f622185fd5
JR <jr1000110@yahoo.fr>
2015-12-04 11:42:58 +0100

    commit

--- game/level/Move.java
+++ game/level/Move.java
@@ -10,30 +10,30 @@ final class Move implements Comparable<Move> {
 		this.x = x;
 		this.y = y;
 	}
-  public Move(Board.Case from, Board.Case to) {
-    this.x = to.x-from.x;
+	public Move(Board.Case from, Board.Case to) {
+		this.x = to.x-from.x;
 		this.y = to.y-from.y;
-  }
-  
-  public Case apply(Case c) {
-    return c.getBoard().getCase(c.x+this.x,c.y+this.y);
-  }
-  
-  @Override
-  public boolean equals(Object o) {
-    if (!this.getClass().equals(o.getClass()))
-      return false;
-    Move m = (Move) o;
-    return this.x==m.x && this.y==m.y;
-  }
-  
-  @Override
-  public int compareTo(Move m) {
-    if (this.x!=m.x)
-      return this.x<m.x?-1:1;
-    if (this.y!=m.y)
-      return this.y<m.y?-1:1;
-    return 0;
-  }
+	}
+	
+	public Case apply(Case c) {
+		return c.getBoard().getCase(c.x+this.x,c.y+this.y);
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (!this.getClass().equals(o.getClass()))
+			return false;
+		Move m = (Move) o;
+		return this.x==m.x && this.y==m.y;
+	}
+	
+	@Override
+	public int compareTo(Move m) {
+		if (this.x!=m.x)
+			return this.x<m.x?-1:1;
+		if (this.y!=m.y)
+			return this.y<m.y?-1:1;
+		return 0;
+	}
 	
 }
--- game/level/board/Board.java
+++ game/level/board/Board.java
@@ -7,9 +7,11 @@ public abstract class Board {
 		return new Case(this,x,y);
 	}
 	
-	// -1 : case hors plateau
-	// 0 : case vide ( traversable )
-	// n>0 : case de sortie du joueur (n-1)
+	/**
+	 * -1 : case hors plateau
+	 * 0 : case vide ( traversable )
+	 * n>0 : case de sortie du joueur (n-1)
+	 */
 	abstract int getContent(int x,int y);
 	
 }
