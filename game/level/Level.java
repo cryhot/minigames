b5a5e4b8da4753deef652771a86f676aef82c8ce
Louis Bal dit Sollier <Louis Bal dit Sollier>
2015-12-04 12:36:12 +0100

    A moi sont : LEVEL ET PAWN

--- game/level/Level.java
+++ game/level/Level.java
@@ -1,5 +1,22 @@
+//Louis
 package game.level;
 
+import java.util.*;
+import game.level.board.Case;
+
 public class Level {
+	
+	private Set<Pawn> pawns;
+	
+	public Pawn getPawnAt(Case c){
+		
+		for(Pawn p :this.pawns){
+			if(p.getCase().equals(c)){
+				return p;
+			}
+		}
+		return null;
+		
+	}
 	
 }
--- game/level/Pawn.java
+++ game/level/Pawn.java
@@ -1,9 +1,14 @@
+//Louis
+
 package game.level;
 
 public class Pawn {
 	private static Move[] moves = {new Move(0,1), new Move(1,0), new Move(-1,0) , new Move(0,-1)};
 	private Case c;
 	
+	public Case getCase(){
+		return this.c;
+	}
 	
 	
 	
