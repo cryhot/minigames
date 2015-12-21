8949e6afae59441991823ebab9369c9246efab75
JR <jr1000110@yahoo.fr>
2015-12-21 17:45:43 +0100

    ajout de javadoc dans Property

--- util/Property.java
+++ util/Property.java
@@ -3,11 +3,28 @@ package util;
 import java.util.Collection;
 import java.util.HashSet;
 
+/** Classe utilitaire permettant de faire des "requêtes" sur des collections d'éléments
+ * Une propriétée définit un critère de validation permettant de faire un choix parmis les éléments de la collection.
+ * @param <E>  le type des éléments sur lesquels portent la propriété
+ * @see #validate
+ */
 public abstract class Property<E>{
 	
-	protected abstract boolean validate (E element);
+	/** Méthode à redéfinir, exprimant le critère de séléction d'un élément.
+	 * Cette méthode doit être :
+	 * <ul>
+	 * 	<li>Constante : divers appels à cette fonction sur un même élément doit renvoyer un résultat unique.</li>
+	 * 	<li>Transitive par rapport à la méthode {@link Object#equals(Object)}, c'est à dire que deux éléments dits égaux doivent renvoyer le même résultat.</li>
+	 * @param element  l'élément à étudier
+	 * @return  <code>true</code> si l'élément valide le crière, <code>false</code> sinon
+	 */
+	protected abstract boolean validate(E element);
 	
-	public final HashSet<E> select(Collection <E> c) {
+	/** Renvoie l'ensemble des éléments de la collection qui vérifient cette propriété.
+	 * @param c  la collection d'éléments sur laquelle porte la requête
+	 * @return  l'ensemble des éléments vérifiant cette propriété
+	 */
+	public final HashSet<E> select(Collection<E> c) {
 		HashSet<E> elements  = new HashSet<E>();
 		for(E e : c)
 			if(validate(e))
@@ -15,6 +32,10 @@ public abstract class Property<E>{
 		return elements;
 	}
 	
+	/** Renvoie un élément de la collection qui vérifie cette propriété.
+	 * @param c  la collection d'éléments sur laquelle porte la requête
+	 * @return  le premier des éléments trouvés vérifiant cette propriété, ou <code>null</code> si aucun élément n'est trouvé
+	 */
 	public final E find(Collection<E> c) {
 		for(E e : c)
 			if(this.validate(e))
@@ -22,6 +43,10 @@ public abstract class Property<E>{
 		return null;
 	}
 	
+	/** Compte le nombre d'éléments de la collection qui vérifient cette propriété.
+	 * @param c  la collection d'éléments sur laquelle porte la requête
+	 * @return  le nombre d'éléments vérifiant cette propriété
+	 */
 	public final int count(Collection<E> c){
 		int n = 0;
 		for(E e : c)
