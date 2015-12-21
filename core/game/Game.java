2b5445ff030a00e2f6f510a0f847d39f35487bf0
JR <jr1000110@yahoo.fr>
2015-12-22 00:30:19 +0100

    compilation fix : Game.java modification de la méthode checkDefeat() : lève une exception lorsqu'il y a match nul

--- core/game/Game.java
+++ core/game/Game.java
@@ -1,106 +1,87 @@
 package core.game;
 
-
-
+import core.exceptions.*;
 
 public class Game {
-	
 	private Level level;
 	private boolean started;
 	
-	public Game(Level l){
+	public Game(Level l) {
 		this.started = false;
 		this.level = l;
 	}
 	
-	Level getLevel(){
-		return this.level;
-	}
-	
-	public void subscribe(Player p){
-		
-		if(this.started){
-			throw new RuntimeException();
-		}
+	public void subscribe(Player p) {
+		if(this.started)
+			throw new GameStateException("le jeu à déjà commencé");
+		if(p==null)
+			throw new NullPointerException();
+		if(this.level.players.contains(p))
+			throw new IllegalArgumentException("joueur déjà inscrit");
 		this.level.players.add(p);
 	}
 	
-	private void initialise(){
-		
-		for(Player p : this.level.players){
-			p.placePawns();		
-		}
+	public Player play() {
+		if(this.started)
+			throw new GameStateException("le jeu a déjà commencé");
+		this.started = true;
+		initialize();
+		return playTurns();
 	}
 	
-	public Player checkDefeat(){
-		
+	private void initialize() {
+		// for(Player p : this.level.players)
+			// p.placePawns(); // à compléter en temps voulu
+	}
+	
+	private Player playTurns() {
 		Player winner = null;
-		for(Player p : this.level.players){
-				if(!p.checkDefeat()){
-					
-					if(winner==null){
-						winner = p;
-					}
-					else {
-						return null;
-					}
-				}
-		}
-		
-		return winner;
-		
-	}
-	
-	
-	private Player playTurn(Player player){
-				
-		for(Player p : this.level.players){
-			if(p.checkPassiveVictory()){
-				return p;
-		}
-					
-		if(player.checkActiveVictory()){
-			return player;
-		}
-		
-		Player winnerByDefeat = checkDefeat();
-		if(winnerByDeafeat!=null){
-			return winnerByDeafeat;
-		}
-			
-		player.playTurn();
-			
-		return null;
-		}	
-	}
-	
-	
-	
-	private Player turns(){
-		Player winner = null;
-		while (true){
+		while (true)
 			for (Player p: this.level.players) {
 				winner = this.playTurn(p);
 				if (winner!=null)
 					return winner;
 			}
-		}
-		return null;
-	}	
-	
-	
-		
-	public Player play(){
-		
-		if(this.started){
-			throw new RuntimeException();
-		}
-		this.started = true;
-		initialise();
-		return turns();
-		
 	}
-
 	
+	private Player playTurn(Player player) {
+		
+		try {
+			Player winnerByDefeat = this.checkDefeat();
+			if (winnerByDefeat!=null)
+				return winnerByDefeat;
+		} catch (DrawException e) {
+			return null;
+		}
+		
+		for(Player p : this.level.players)
+			if(p.checkPassiveVictory())
+				return p;
+		
+		if(player.checkActiveVictory())
+			return player;
+		
+		player.playTurn();
+		
+		return null;
+	}
+	
+	private Player checkDefeat() throws DrawException {
+		Player winner = null;
+		for(Player p : this.level.players)
+			if(!p.checkDefeat()) {
+				if(winner==null || winner.equals(p))
+					winner = p;
+				else
+					return null; // au moins 2 joueurs en jeu
+			}
+		if (winner==null) // tout le monde a perdu
+			throw new DrawException();
+		return winner; // un seul joueur encore en jeu
+	}
+	
+	Level getLevel() {
+		return this.level;
+	}
 	
 }
