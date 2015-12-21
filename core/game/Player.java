bbca8f15ff417b6902dd676f34d55b34259a27eb
JR <jr1000110@yahoo.fr>
2015-12-21 20:04:28 +0100

    * ajout des conditions de victoire active, victoire passive et défaite dans Player * ajout d'une 4ème fonctionnalité dans Property (exists)

--- core/game/Player.java
+++ core/game/Player.java
@@ -3,27 +3,80 @@ package core.game;
 import java.util.Set;
 import java.util.HashSet;
 
+import core.board.Case;
 import util.Property;
 
-public class Player {
+public abstract class Player {
 	final Level level;
-	public final int index;
 	
-	private Player(Error nimportequoi){this.level=null;this.index=-1;} // CONSTRUCTEUR BIDON POUR QUE LE PROGRAMME COMPILE !
+	public Player(Game g) {
+		this.level = g.getLevel();
+		g.subscribe(this);
+	}
 	
-	/**
-	 * Renvoie tous les pions de ce joueur.
+	/** Renvoie tous les pions de ce joueur.
 	 * @return  l'ensemble de tous les pions appartenant à ce joueur
 	 */
 	public Set<Pawn> getPawns() {
-		final Player player = this;
+		final Player this_ = this;
 		return this.level.getPawns( new Property<Pawn>() {
-			protected boolean validate(Pawn p) { return p.getOwner().equals(player); }
+			protected boolean validate(Pawn p) { return p.getOwner().equals(this_); }
 		} );
 	}
 	
 	Level getLevel() {
 		return this.level;
+	}
+	
+	public int getIndex() {
+		return this.level.getIndex(this);
+	}
+	
+	/** Méthode appelée par la mécanique du jeu, initialisant le placement des pions.
+	 * @param cases  les cases où placer les pions;
+	 * @param pawns  les pions à placer
+	 */
+	void placePawns(Set<Case> cases,Set<Pawn> pawns) {
+		
+	}
+	
+	/** Méthode appelée par la mécanique du jeu, déroulant un tour de jeu.
+	 */
+	void playTurn() {
+		
+	}
+	
+	/** Méthode appelée par la mécanique du jeu, vérifiant une victoire active de ce joueur.
+	 * Le joueur a gagné activement s'il possède un pion pouvant gagner activement et situé sur une case de sortie.
+	 * @return  <code>true</code> si le joueur gagne activement.
+	 */
+	boolean checkActiveVictory() {
+		final Player this_ = this;
+		return new Property<Pawn>() {
+			protected boolean validate(Pawn p) { return p.getOwner().equals(this_) && p.induceVictory(); }
+		}.exists(this.getLevel().pawns);
+	}
+	
+	/** Méthode appelée par la mécanique du jeu, vérifiant une victoire passive de ce joueur.
+	 * Le joueur a gagné passivement si tous ses pions empêchant une victoire passive ont étés éliminés.
+	 * @return  <code>true</code> si le joueur gagne passivement.
+	 */
+	boolean checkPassiveVictory() {
+		final Player this_ = this;
+		return !( new Property<Pawn>() {
+			protected boolean validate(Pawn p) { return p.getOwner().equals(this_) && p.inhibitVictory(); }
+		}.exists(this.getLevel().pawns) );
+	}
+	
+	/** Méthode appelée par la mécanique du jeu, vérifiant une défaite de ce joueur.
+	 * Le joueur a perdu s'il n'a plus de pions pouvant gagner activement en jeu.
+	 * @return  <code>true</code> si le joueur perd.
+	 */
+	boolean checkDefeat() {
+		final Player this_ = this;
+		return !( new Property<Pawn>() {
+			protected boolean validate(Pawn p) { return p.getOwner().equals(this_) && p.inhibitDefeat(); }
+		}.exists(this.getLevel().pawns) );
 	}
 	
 }
