2cff25cd5c20b7d7c633e43b3f26b56ddbcf39a0
JR <jr1000110@yahoo.fr>
2015-12-30 17:14:28 +0100

    ajout du PlayerControler faisant le lien entre Game et l'interface

---
+++ core/game/PlayerControler.java
@@ -0,0 +1,15 @@
+package core.game;
+
+public class PlayerControler extends PlayerViewer {
+	
+	/** Appelé par Game. */
+	public setPlayer(Player p);
+	
+	/** Appelé par Game. */
+	public setPlayer(Player p);
+	
+	
+	
+	
+	
+}
