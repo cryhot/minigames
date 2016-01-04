780fede1bb2cb5644da42cbcb19b11a4cfa1276c
Louis Bal dit Sollier <Louis Bal dit Sollier>
2016-01-04 23:51:43 +0100

    Finition Reader et Interface Reader

--- InterfaceReader.java
+++ InterfaceReader.java
@@ -6,11 +6,23 @@ import core.board.Case;
 
 public class InterfaceReader extends PlayerControler{
 	
-	private Reader reader;
-	private ArrayList<String> coordonnates = reader.getMoveOfReader();
-	private int position = 0;
+	private Reader reader = new Reader();
+	private ArrayList<String> coordonnates1 = reader.getMoveOfReader(1);
+	private ArrayList<String> coordonnates2 = reader.getMoveOfReader(2);	
+	private int position1 = 1;
+	private int position2 = 2;
+	private int player;
+	
+	public InterfaceReader(Reader r, int n){
+		this.reader = r;
+		this.player = n;
+	}
+	
+	
+	
 	
 	protected ArrayList<Case> initialCases(){
+		
 		ArrayList<Soul> a1 = reader.getInitialPosition(1);
 		ArrayList<Soul> a2 = reader.getInitialPosition(2);
 		List<Pawn> pawns = this.getInitialPawns();
@@ -22,40 +34,64 @@ public class InterfaceReader extends PlayerControler{
 		Iterator<Soul> it1 = a1.iterator();
 		Iterator<Soul> it2 = a2.iterator();
 		for(Case c : initCases ) {
-			if(it1.hasNext()){
-				Soul soul1 = it1.next();
-				for(int i=0;i<pawns.size();i++){
-					Soul s1 = this.getSoul(pawns.get(i));
-					if(s1.equals(soul1)){
-						cases.remove(i);
-						cases.add(i,c);
-						pawns.remove(i);
-						pawns.add(i,null);
-						break;
-					}
-				}	
+			if(player==0){
+				if(it1.hasNext()){
+					Soul soul1 = it1.next();
+					for(int i=0;i<pawns.size();i++){
+						Soul s1 = this.getSoul(pawns.get(i));
+						if(s1.equals(soul1)){
+							cases.remove(i);
+							cases.add(i,c);
+							pawns.remove(i);
+							pawns.add(i,null);
+							break;
+						}
+					}	
+				}
 			}
-			else if(it2.hasNext()){
-				Soul soul2 = it2.next();
-				for(int i=0;i<pawns.size();i++){
-					Soul s2 = this.getSoul(pawns.get(i));
-					if(s2.equals(soul2)){
-						cases.remove(i);
-						cases.add(i,c);
-						pawns.remove(i);
-						pawns.add(i,null);
-						break;
-					}
-				}				
+			else if(player==1){
+				if(it2.hasNext()){
+					Soul soul2 = it2.next();
+					for(int i=0;i<pawns.size();i++){
+						Soul s2 = this.getSoul(pawns.get(i));
+						if(s2.equals(soul2)){
+							cases.remove(i);
+							cases.add(i,c);
+							pawns.remove(i);
+							pawns.add(i,null);
+							break;
+						}
+					}				
+				}
 			}
 		}
 		return cases;
 	}
 
 	protected Case selectCase(){
-		if(coordonnates.get(this.position).equals("1")||coordonnates.get(this.position).equals("2"))
-			this.position++;
-		return coordonnates(coordonnates.get(this.position));
+		if(player==0){
+			try{
+				position1++;
+				return coordonnates(coordonnates1.get(this.position1));
+				
+			}
+			catch(Exception e){
+				return null;
+			}
+			
+		}
+		if(player==1){
+			
+			try{
+				position2++;
+				return coordonnates(coordonnates2.get(this.position2));
+			}
+			catch(Exception e){
+				return null;
+			}
+			
+		}
+		return null;
 	}
 	
 	
@@ -95,6 +131,20 @@ public class InterfaceReader extends PlayerControler{
 		}
 		return this.getBoard().getCase(this.getBoard().getXMin()+x-1,this.getBoard().getYMin()+y-1);
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
